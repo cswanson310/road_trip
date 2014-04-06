b71753a06f2dc83835e6168331fecf3b31df0917
Charlie Swanson <cswanson310@gmail.com>
2014-04-05 21:41:04 -0400

    forgot to commit for a while...

---
+++ app/controllers/header_controller.rb
@@ -0,0 +1,7 @@
+class HeaderController < ApplicationController
+
+  layout false
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ RoadTrip::Application.routes.draw do
 
   root 'welcome#index'
 
+  resources 'welcome', only: :index
+  resources 'header', only: :index
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
